f8c97512705a04772e091c7bce51d3eae5a94ca8
Amila Welihinda <amilajack@gmail.com>
2017-06-11 00:12:22 -0700

    Refactor index.js

--- index.js
+++ index.js
@@ -1,80 +1,93 @@
-const sqlite = require('sqlite3'),
-    fs = require('fs'),
-    path = require('path'),
-    mkdirp = require('mkdirp');
+const sqlite = require('sqlite3');
+const fs = require('fs');
+const path = require('path');
+const mkdirp = require('mkdirp');
 
 module.exports = sqliteJSON;
 
 function sqliteJSON(database) {
-    if (!(this instanceof sqliteJSON))
-        return new sqliteJSON(database);
+  if (!(this instanceof sqliteJSON)) {
+    return new sqliteJSON(database);
+  }
 
-    this.client = (database instanceof sqlite.Database) ? database : new sqlite.Database(database);
+  this.client = database instanceof sqlite.Database
+    ? database
+    : new sqlite.Database(database);
 
-    return this;
+  return this;
 }
 
-sqliteJSON.prototype.json = function(sql, options, cb) {
+sqliteJSON.prototype.json = function (sql, options, cb) {
+  if (options instanceof Function) {
+    cb = options;
+  }
 
-    if (options instanceof(Function))
-        cb = options;
+  if (sql instanceof Object) {
+    options = sql;
+    sql = null;
+  }
 
-    if (sql instanceof(Object)) {
-        options = sql;
-        sql = null;
+  if (!sql) {
+    // make sure the key is in the output
+    if (
+      options.key &&
+      options.columns &&
+      options.columns.indexOf(options.key) < 0
+    ) {
+      options.columns.push(options.key);
     }
 
-    if (!sql) {
-        // make sure the key is in the output
-        if (options.key && options.columns && options.columns.indexOf(options.key) < 0)
-            options.columns.push(options.key);
+    const columns = options.columns ? options.columns.join(', ') : '*';
+    const where = options.where ? ` WHERE ${options.where}` : '';
 
-        const columns = (options.columns) ? options.columns.join(', ') : '*',
-            where = (options.where) ? ' WHERE ' + options.where : '';
+    sql = `SELECT ${columns} FROM ${options.table}${where};`;
+  }
 
-        sql = 'SELECT ' + columns + ' FROM ' + options.table + where + ';';
+  this.client.all(sql, (err, data) => {
+    if (err) {
+      cb(err);
+      return;
     }
 
-    this.client.all(sql, function(err, data) {
-        if (err) {
-            cb(err);
-            return;
-        }
+    if (options.key) {
+      data = data.reduce((obj, item) => {
+        obj[item[options.key]] = item;
+        return obj;
+      }, {});
+    }
 
-        if (options.key)
-            data = data.reduce(function(obj, item) { obj[item[options.key]] = item; return obj; }, {});
+    cb(null, JSON.stringify(data));
+  });
 
-        cb(null, JSON.stringify(data));
-    });
-
-    return this;
+  return this;
 };
 
-sqliteJSON.prototype.save = function(table, filename, cb) {
-    this.json(table, function(err, data) {
+sqliteJSON.prototype.save = function (table, filename, cb) {
+  this.json(table, (err, data) => {
+    if (err) cb(err);
+
+    mkdirp(path.dirname(filename), err => {
+      if (err) cb(err);
+
+      fs.writeFile(filename, data, err => {
         if (err) cb(err);
-
-        mkdirp(path.dirname(filename), function(err) {
-            if (err) cb(err);
-
-            fs.writeFile(filename, data, function(err) {
-                if (err) cb(err);
-                else cb(null, data);
-            });
-        });
+        else cb(null, data);
+      });
     });
+  });
 
-    return this;
+  return this;
 };
 
-sqliteJSON.prototype.tables = function(cb) {
-    const query = "SELECT name FROM sqlite_master WHERE type='table'";
+sqliteJSON.prototype.tables = function (cb) {
+  const query = "SELECT name FROM sqlite_master WHERE type='table'";
 
-    this.client.all(query, function (err, tables) {
-        if (err)
-            cb(err);
-        cb(null, tables.map(function (t) { return t.name; }));
-    });
+  this.client.all(query, (err, tables) => {
+    if (err) {
+      cb(err);
+    }
+    cb(null, tables.map(t => t.name));
+  });
 
-    return this;
+  return this;
 };
